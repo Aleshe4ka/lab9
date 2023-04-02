79e670d36c13943f1859103fab2cc4ba9bc1a141
nastyapere <pavevaa1@mail.ru>
2023-04-02 22:28:33 +0300

    HomeScreenn

--- app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
@@ -22,11 +22,11 @@ fun HomeScreen(viewModel: MainViewModel) {
 val posts: List<PostModel> by viewModel.allPosts.observeAsState(listOf())
 
     LazyColumn(
-        modifier = Modifier.background(color = MaterialTheme.colors.secondary)){
-        items(posts){
-            if (it.type == PostType.TEXT){
+        modifier = Modifier.background(color = MaterialTheme.colors.secondary)) {
+        items(posts) {
+            if (it.type == PostType.TEXT) {
                 TextPost(it)
-            } else{
+            } else {
                 ImagePost(it)
             }
             Spacer(modifier = Modifier.height(6.dp))
